85cdcd6a8cc9b4970c01b8e59b480c0f44a3e335
Andreas Hümmerich <andreas@huemmerich-web.de>
2022-03-03 18:17:53 +0100

    chore: fix develop version

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "version = $1"
+versionParameter=$1
+echo "version = $versionParameter"
 
 # Get version number from version tag
-JAR_VERSION=`echo $1 | cut -d'v' -f2`
+
+if [[ ${versionParameter} == v* ]]; then
+  JAR_VERSION=`echo $1 | cut -d'v' -f2`
+else
+  JAR_VERSION=${versionParameter}
+fi
 echo "jar = $JAR_VERSION"
 
 ./gradlew -si -Pversion=${JAR_VERSION} build sourcesJar javadocJar publish
